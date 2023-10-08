4d406cb2143450d5f7e37e3f5a6bd4360db79486
Sumeet Wadile <sumeetwd25@gmail.com>
2023-10-09 00:44:56 +0530

    Added Solution - GfG to GitHub

---
+++ School/Print 1 to n without using loops/print-1-to-n-without-using-loops.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int N=sc.nextInt();
+			
+            Solution ob = new Solution();
+            ob.printTillN(N);
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static void printTillN(int N){
+        // code here
+        int num=1;
+        print(num,N);
+    }
+    
+    public static void print(int num, int N){
+        System.out.print(num+" ");
+        if(num==N) return;
+        print(++num,N);
+    }
+}
