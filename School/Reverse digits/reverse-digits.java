c5412c9f6246f368862325bbe501ddc3ee820e29
Sumeet Wadile <sumeetwd25@gmail.com>
2023-10-08 23:11:01 +0530

    Added Solution - GfG to GitHub

---
+++ School/Reverse digits/reverse-digits.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());            
+            Solution ob = new Solution();
+            long ans = ob.reverse_digit(n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public long reverse_digit(long n)
+    {
+        // Code here
+        String num = Long.toString(n);
+        return Long.parseLong(new StringBuilder(num).reverse().toString());
+    }
+}
