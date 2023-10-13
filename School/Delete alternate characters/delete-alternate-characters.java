1993d53b7ae3ccdc69dab9acf681da0a6aeaf389
Sumeet Wadile <sumeetwd25@gmail.com>
2023-10-13 23:43:02 +0530

    Added Solution - GfG to GitHub

---
+++ School/Delete alternate characters/delete-alternate-characters.java
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+// Initial template for Java
+
+import java.util.*;
+import java.io.*;
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S = read.readLine();
+            Solution ob = new Solution();
+
+            System.out.println(ob.delAlternate(S));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function template for Java
+
+class Solution {
+    static String delAlternate(String S) {
+        // code here
+        char[] arr = S.toCharArray();
+        String altStr="";
+        for(int i=0; i<arr.length; i++){
+            if(i%2==0) altStr+=arr[i]; 
+        }
+        return altStr;
+    }
+}
