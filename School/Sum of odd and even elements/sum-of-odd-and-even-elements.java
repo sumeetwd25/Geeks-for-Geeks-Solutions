93153e65e628eebae7158179217c4c1b32ecc2bc
Sumeet Wadile <sumeetwd25@gmail.com>
2023-10-31 17:34:44 +0530

    Added Solution - GfG to GitHub

---
+++ School/Sum of odd and even elements/sum-of-odd-and-even-elements.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            int[] ans = ob.find_sum(n);
+            for(int i = 0; i < ans.length; i++)
+            {
+                System.out.print(ans[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int[] find_sum(int n)
+    {
+        // Code here
+        int oddSum=0, evenSum=0;
+        for(int i=1; i<=n; i++){
+            if(i%2==0) evenSum+=i;
+            else oddSum+=i;
+        }
+        return new int[]{oddSum, evenSum};
+    }
+}
