15f0c7e12c01b307cb692f9f148ba9864e254d83
Sumeet Wadile <sumeetwd25@gmail.com>
2023-10-31 16:36:46 +0530

    Added Solution - GfG to GitHub

---
+++ School/Java loops | Set 1/java-loops-set-1.java
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+        while(t-- > 0)
+        {
+            int N = scn.nextInt();
+
+            Solution ob = new Solution();
+            ArrayList<Integer> sum = ob.getSum(N);
+            System.out.println(sum.get(0)+" "+sum.get(1));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static ArrayList<Integer> getSum(int N){
+        // code here
+        int evenSum=0;
+        int oddSum=0;
+        for(int i=1; i<=N; i++){
+            if(i%2==0) evenSum+=i;
+            else oddSum+=i;
+        }
+        ArrayList<Integer> alist = new ArrayList<>();
+        alist.add(evenSum);
+        alist.add(oddSum);
+        return alist;
+    }
+}
