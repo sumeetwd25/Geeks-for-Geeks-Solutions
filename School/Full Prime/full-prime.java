e9bcdcc7ff3d2faf464d0195a905f545475f48d7
Sumeet Wadile <sumeetwd25@gmail.com>
2023-10-31 16:04:57 +0530

    Update full-prime.java

--- School/Full Prime/full-prime.java
+++ School/Full Prime/full-prime.java
@@ -44,21 +44,10 @@ class Solution{
 	}
 	
     static boolean isPrime(int N){
-        if (N <= 1) {
-            return false;
-        }
-        if (N <= 3) {
-            return true;
-        }
-        if (N % 2 == 0 || N % 3 == 0) {
-            return false;
-        }
-        for (int i = 5; i * i <= N; i += 6) {
-            if (N % i == 0 || N % (i + 2) == 0) {
-                return false;
-            }
+        if(N<2) return false;
+        for(int i=2; i<=Math.sqrt(N); i++){
+            if(N%i==0) return false;
         }
         return true;
     }
-
 }
